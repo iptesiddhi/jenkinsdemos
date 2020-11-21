fc7f853f31287e905ace37d06feba1981ebd8b9a
Siddhi <iptesiddhi78@gmail.com>
2020-11-21 16:48:00 +0530

    commit6 by siddhi

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello
 {
 	public static void main(String[] args)
 	{
+		int x=1/0;
 		System.out.println("Hello World..");
 		System.out.println("Hello Universe..cskdncskdnck");
 System.out.println("Hello World..");
